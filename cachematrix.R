9c9068c8279d9300afdd524267b67592990e31fc
James Kwan <james.mh.kwan@gmail.com>
2014-12-20 11:23:24 -0700

    Assignment 2 commit (done)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makeCacheMatrix creates a "matrix", and caches
+## the inverse of the matrix
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+InvrsdMtrx <- NULL  ## sets Invrsd Matrix ro NULL
+GtMtrx <- function() {x} ## returns the original matrix
+##will store invesred matrix calculated by cacheSolve
+StInvrsdMtrx <- function(csInvsrdMtrx) { InvrsdMtrx <<- csInvsrdMtrx} 
+GtInvrsdMtrx <- function() {InvrsdMtrx} ## returns cached inversed matrix
 
+list(GtMtrx = GtMtrx,  StInvrsdMtrx=StInvrsdMtrx,GtInvrsdMtrx=GtInvrsdMtrx )
 }
 
 
-## Write a short comment describing this function
+## cachceSolve: takes inverse matrix from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  InvrsdMtrx <- x$GtInvrsdMtrx() ## gets cached inversed matrix
+  
+  if (!is.null(InvrsdMtrx)) { #if cachced inversed matrix is not NULL (i.e. it exists)
+    message("getting cached data")
+    return(InvrsdMtrx) ## return the existing cached inversed matrix
+  }
+  ## Else...
+  Mtrx <- x$GtMtrx() ## gets original matrix (not inversed)
+  InvrsdMtrx <- solve(Mtrx) ##perform inverse calculation
+  x$StInvrsdMtrx(InvrsdMtrx) ## cache cacluated inverse matrix
+  InvrsdMtrx ## returned calcualted inverse matrix
+  
+  
+  ## Return a matrix that is the inverse of 'x'
 }
